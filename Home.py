85e6d45fe6dc8c8cb0505e0443f053e5aeabbc57
Buddhika Madusanka <buddhikadevelopmetn@gmail.com>
2025-06-17 16:56:38 +0530

    Setup basic file structure and home page with the basic funcion cration without function body

---
+++ Home.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from functions._add_new_transaction import add_new_income_transaction_, add_new_expense_transaction
+
+st.title("Add new Transaction")
+
+with st.container():
+
+    transaction_amount_input, transaction_type_input = st.columns([2,1])
+
+    with transaction_amount_input:
+        transaction_amount = st.number_input("Tranaction Amount")
+    with transaction_type_input:
+        transaction_type = st.selectbox("Transaction Type", ["Income", "Expense"])
+
+    if transaction_type.lower() == "income":
+        income_resource = st.selectbox("Income Source", ["Job", "Business Income", "Investment"])
+        income_note = st.text_area("Additional Notes")
+
+        income_add = st.button("Add new Income")
+
+        if income_add:
+            add_new_income_transaction_()
+    
+    if transaction_type.lower() == "expense":
+
+        expense_tag_,expense_reason_ = st.columns([2,5])
+
+        with expense_tag_:
+            expense_tag = st.selectbox("Expense Category", ["Monthly Bills", "Rent", "Gods", "Subscriptions"])
+        with expense_reason_:
+            expense_reason = st.text_input("Expense Reasone")
+
+        expense_additional_note = st.text_area("Additional note")
+        expense_add = st.button("Add new Expense")
+
+        if expense_add:
+            add_new_expense_transaction()
---
+++ functions/_add_new_transaction.py
@@ -0,0 +1,7 @@
+
+def add_new_income_transaction_():
+    print("this is income transaction")
+
+
+def add_new_expense_transaction():
+    print("this is expense tranaction")

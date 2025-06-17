8a2d4f87e7acfac8bae7a27f5fa4d0dd567c215b
Buddhika Madusanka <buddhikadevelopmetn@gmail.com>
2025-06-17 18:00:42 +0530

    Setup the database connection functionality

--- utils/_database_connection.py
+++ utils/_database_connection.py
@@ -1,8 +1,32 @@
 import os
 import psycopg2
-from dotenv import load_dotenv
+from psycopg2.extensions import connection
+import logging
 
-load_dotenv()
+# setup basic logging
+logging.basicConfig(level= logging.INFO)
+logger = logging.getLogger(__name__)
 
-def database_connection():
-    print("This is my database connection")
+def database_connection() -> connection | None :
+    """
+    Extablished a connection to the postgreSQL using enviorenment variables
+
+    returns:
+        A psycopg2 object if successfull, or None if the connection fails
+    """
+    
+    try:
+        conn = psycopg2.connect(
+            host = os.getenv("db_host"),
+            port = os.getenv("db_port"),
+            database = os.getenv("database"),
+            user = os.getenv("db_user"),
+            password = os.getenv("db_password")
+        )
+        
+        logger.info("Established the database connection successfully.")
+        return conn
+    
+    except Exception as e:
+        logger.error(f"Something bad happen while databse conneting process... {e}")
+        print(f"Something went wrong in database connection... {e}")

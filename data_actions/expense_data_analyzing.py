fb6e7a05d4864bc4f07931888991d44c671157cc
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-06-20 18:15:08 +0530

    Show the expense analitics and related functions

--- data_actions/expense_data_analyzing.py
+++ data_actions/expense_data_analyzing.py
@@ -1,8 +1,25 @@
+import pandas as pd
 from utils._database_connection import databse_connection
 from utils._database_actions import get_all_expense_details
 
 conn = databse_connection()
 expense = get_all_expense_details(conn)
 
-def analyze_monthly_expense():
-    print("thjis is analyze function")
+def analyze_monthly_expense() -> pd.DataFrame:
+    columns = ["Date", "Amount"]
+    monthly_expense_details = expense[columns]
+    monthly_expense_details["Date"] = monthly_expense_details["Date"].dt.to_timestamp().dt.to_period("M")
+
+    monthly_expense_details = monthly_expense_details.groupby(by="Date")["Amount"].sum().reset_index()
+
+    monthly_expense_details["Month"] = monthly_expense_details["Date"].dt.to_timestamp().dt.month_name()
+    monthly_expense_details["Year"] = monthly_expense_details["Date"].dt.to_timestamp().dt.year
+
+    monthly_expense_details = monthly_expense_details.sort_values(by="Date", ascending= False).reset_index(drop= True)
+
+    return monthly_expense_details
+
+
+def monthly_total_expense_value():
+    expense_details = analyze_monthly_expense()
+    return expense_details.iloc[0]["Amount"]
--- pages/Expense.py
+++ pages/Expense.py
@@ -2,14 +2,36 @@ import pandas as pd
 import streamlit as st
 from utils._database_connection import databse_connection
 from utils._database_actions import get_all_expense_details
-from data_actions.expense_data_analyzing import get_all_expense_details
+from data_actions.expense_data_analyzing import analyze_monthly_expense, monthly_total_expense_value
 
 conn = databse_connection()
 expense_database_details=  get_all_expense_details(conn)
 expense_titles = ["Transaction Date", "Transaction Amount", "Expense Category", "Expense Reason", "Expense Additional Note"]
 expense_details = pd.DataFrame(expense_database_details, columns= expense_titles)
 
-get_all_expense_details()
+# generate mean value
+expense_monthly_mean = expense_details["Transaction Amount"].mean()
+
+monthly_total_expense = monthly_total_expense_value()
+expense_analyze_details = analyze_monthly_expense()
+
+st.title("Expense Details")
+
+# show statistics related to monthly income
+monthly_total_expense_ui, averate_expense_value = st.columns(2)
+
+with monthly_total_expense_value:
+    with st.container(border= True):
+        st.text("Total Of Monthly Expense")
+        st.title(f"rs.{monthly_total_expense:.2f}")
+
+with averate_expense_value:
+    with st.container(border= True):
+        st.text("Average expense")
+        st.title(f"rs.{expense_monthly_mean:.2f}")
+
+st.title("Monethly Expense Details")
+st.dataframe(expense_analyze_details)
 
 st.title("Expense Details")
 st.dataframe(expense_details)
